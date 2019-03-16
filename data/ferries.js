f404bf2d55800c3bd32207f2dad98991b4a33324
Stop-SB50 <48577351+Stop-SB50@users.noreply.github.com>
2019-03-15 17:18:03 -0700

    Add files via upload

---
+++ data/ferries.js
@@ -0,0 +1,27 @@
+var ferry_terminals = {
+"type": "FeatureCollection",
+"name": "ferries",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "stop_name": "Marina Bay Yacht Harbor, Richmond CA" }, "geometry": { "type": "Point", "coordinates": [ -122.354316, 37.913659 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Tiburon-Sam's Anchor Cafe" }, "geometry": { "type": "Point", "coordinates": [ -122.4565526, 37.8727469 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Trident Restaurant Sausalito, CA" }, "geometry": { "type": "Point", "coordinates": [ -122.4786033, 37.8538049 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco-Hyde Street Pier" }, "geometry": { "type": "Point", "coordinates": [ -122.4214965, 37.8091991 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Pier 1.5" }, "geometry": { "type": "Point", "coordinates": [ -122.3950251, 37.7974815 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Fransisco Pier 40 " }, "geometry": { "type": "Point", "coordinates": [ -122.3876122, 37.7818831 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Berkeley Marina, Berkeley CA" }, "geometry": { "type": "Point", "coordinates": [ -122.313931843619997, 37.864539695513002 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Alameda Main Street" }, "geometry": { "type": "Point", "coordinates": [ -122.2941569, 37.79115331 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Harbor Bay Ferry Terminal" }, "geometry": { "type": "Point", "coordinates": [ -122.256966, 37.736528 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Mare Island Terminal" }, "geometry": { "type": "Point", "coordinates": [ -122.2687821, 38.10131755 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Oakland Terminal" }, "geometry": { "type": "Point", "coordinates": [ -122.279777, 37.795032 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Pier 41" }, "geometry": { "type": "Point", "coordinates": [ -122.412272, 37.809594 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Richmond" }, "geometry": { "type": "Point", "coordinates": [ -122.359051, 37.909093 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Ferry Building" }, "geometry": { "type": "Point", "coordinates": [ -122.3933799, 37.79548653 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Ferry Building (Gate B)" }, "geometry": { "type": "Point", "coordinates": [ -122.393209, 37.796869 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Ferry Building (Gate E)" }, "geometry": { "type": "Point", "coordinates": [ -122.391612, 37.795106 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Ferry Building (Gate F)" }, "geometry": { "type": "Point", "coordinates": [ -122.39107, 37.79472 ] } },
+{ "type": "Feature", "properties": { "stop_name": "San Francisco Ferry Building (Gate G)" }, "geometry": { "type": "Point", "coordinates": [ -122.39079, 37.79436 ] } },
+{ "type": "Feature", "properties": { "stop_name": "South San Francisco" }, "geometry": { "type": "Point", "coordinates": [ -122.377245, 37.662676 ] } },
+{ "type": "Feature", "properties": { "stop_name": "Vallejo Ferry Terminal" }, "geometry": { "type": "Point", "coordinates": [ -122.2630766, 38.09988432 ] } }
+]
+}
